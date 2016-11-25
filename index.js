adab070dadd255f626f47dff3a0f607c0ed5c05c
Anton Niklasson <niklasson.anton@gmail.com>
2016-11-25 22:32:07 +0100

    Enter to open information about the search hit.

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-'use strict';
 const alfy = require('alfy');
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+alfy.fetch('sl.se/api/TypeAhead/Find/' + alfy.input)
+	.then(response => {
+		const items = response.data
+			.slice(0, 10)
+			.filter(hit => {
+				return hit.Type === 'station';
+			})
+			.map(hit => {
+				return {
+					title: hit.Name,
+					subtitle: hit.Type,
+					arg: `http://sl.se/#/Realtime/${encodeURIComponent(hit.Name)}/${hit.SiteId}`
+				};
+			});
+
+		alfy.output(items);
+	});
